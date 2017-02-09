9ed3e7bc1afd0ef1c4bb1095ceaacd83d395eb44
Brad Cupp <bradjcupp@gmail.com>
2017-02-09 16:57:33 -0500

    Project complete

--- js/script.js
+++ js/script.js
@@ -22,14 +22,32 @@ function toCart() {
     var itemName = itemNameInput();
     var itemPrice = itemPriceInput();
     var itemQuantity = itemQuantityInput();
-    //create new <li>
+    //create new <li> and a class for the <li>
     var newLi = document.createElement('li');
-    //add items together to then send to a new <ul><li>
-    var newLine = ('Item: ' + itemName + " Qty: " + itemQuantity + "     Price: $" + (itemPrice * itemQuantity));
+    newLi.className = 'newLi';
     //add newLine to text content of new <li> created above
-    newLi.textContent = newLine;
+    newLi.textContent = '';
     //add items to ID 'listItem' as a new <li>
     reciept.appendChild(newLi);
+    //create span elements and added their classes
+    var spanElQty = document.createElement('span');
+    spanElQty.className = 'spanElQty';
+    var spanElName = document.createElement('span');
+    spanElName.className = 'spanElName';
+    var spanElPrice = document.createElement('span');
+    spanElPrice.className = 'spanElPrice';
+
+    //Strings for receiept line
+    var spanQty = (itemQuantity+'  ');
+    var spanName = ('  '+itemName);
+    var spanPrice = ('$'+itemPrice*itemQuantity);
+    //Assign strings to text content of spans and add spans to <li>
+    spanElQty.textContent = spanQty;
+    newLi.appendChild(spanElQty);
+    spanElName.textContent = spanName;
+    newLi.appendChild(spanElName);
+    spanElPrice.textContent = spanPrice;
+    newLi.appendChild(spanElPrice);
     //created a variable to hold total value of price*quantity
     var total = itemPrice*itemQuantity;
     //pushing total items into an array
@@ -47,6 +65,6 @@ function toCart() {
     recieptTotal.appendChild(totalLi);
     //getElementById to store and later make hidden reciept <div> not hidden.
     var notHidden = document.getElementById('recieptList');
+    //change the visbity styling from invisible to visible
     notHidden.style.visibility = 'visible';
-
 }
